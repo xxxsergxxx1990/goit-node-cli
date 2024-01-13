48c4da75123223c4d74540755427dc5e19cd52f1
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2024-01-13 20:29:21 +0200

    отсылаю домашку

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import { program } from "commander";
+
+
+const { Command } = require("commander");
+const contacts = require("./contacts.js");
+const program = new Command();
+
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -6,32 +13,37 @@ program
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
 
-program.parse();
+program.parse(process.options);
 
 const options = program.opts();
 
 // TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      // ...
-      break;
-
-    case "get":
-      // ... id
-      break;
-
-    case "add":
-      // ... name email phone
-      break;
-
-    case "remove":
-      // ... id
-      break;
-
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+    switch (action) {
+      case "list":
+        const allContacts = await contacts.listContacts();
+        return console.log(allContacts);
+        break;
+  
+      case "get":
+        const contactById = await contacts.getContactById(id);
+        return console.log(contactById);
+        break;
+  
+      case "add":
+        const addedContact = await contacts.addContact(name, email, phone);
+        return console.log(addedContact);
+        break;
+  
+      case "remove":
+        const removeContact = await contacts.removeContact(id);
+        return console.log(removeContact);
+        break;
+  
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
   }
-}
+  
+  invokeAction(options);
 
-invokeAction(options);
